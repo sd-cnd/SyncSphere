1fcf876ddb33b7f7bee352abeefd00c986bec4d8
SUMIT DEY <sumitdey.cnd@gmail.com>
2024-01-16 20:10:40 +0530

    Update navbar.tsx

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -27,7 +27,7 @@ export const Navbar = () => {
         </FormPopover>
       </div>
       <div className="ml-auto flex items-center gap-x-2">
-        <div className="bg-slate-600 border rounded-sm">
+        <div className="bg-slate-600 border rounded-md">
         <OrganizationSwitcher
           hidePersonal
           afterCreateOrganizationUrl="/organization/:id"
